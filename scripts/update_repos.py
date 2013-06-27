6a73645338aeff002a7506f6d181d2369744eb0f
jeffreyksmithjr <jeffreyksmithjr@gmail.com>
2013-06-28 00:20:05 +0800

    working out the bugs in hg to git translation

--- scripts/update_repos.py
+++ scripts/update_repos.py
@@ -7,7 +7,11 @@ def push(ui, repo, **kwargs):
 	try:
 		response = urllib2.urlopen('http://opendns.com', timeout=1)
 
+		# update BitBucket
 		hg_proc = subprocess.call('hg push main', shell=True)
+		# move bookmark forward for Git
+		bm_proc = subprocess.call('hg bookmark -f master', shell=True)
+		# update GitHub
 		gh_proc = subprocess.call('hg push mirror', shell=True)
 	except:
 		print 'No internet, so not pushing to the servers'
